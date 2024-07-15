684f9bdd28f0e78b7d565b3919023972bdad5356
Benjamin Benny <35957758+Benjamin-Benny@users.noreply.github.com>
2024-07-15 13:10:07 +0530

    initial commit with gitignore and starter files

---
+++ src/test/java/com/project/system/admin/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.system.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
